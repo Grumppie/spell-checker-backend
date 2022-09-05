f65f48b192971963f6ac210f816a33ce8895d85a
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-09-05 19:47:42 +0530

    added logic for tacking number of different logs

--- Routes/addLog.js
+++ Routes/addLog.js
@@ -6,9 +6,36 @@ router.post('/', async (req, res) => {
     const Id = req.body.Id
     const Log = req.body.Log
     try {
+        let ob = await Object.find({ Id: Id })
+        ob = ob[0]
+        if (ob['Logs']) {
+            let match = false
+            const newOb = ob['Logs'].map(item => {
+                console.log(item, Log);
+                if (item['Log'] === Log) {
+                    match = true
+                    return {
+                        Log,
+                        Num: item.Num ? item.Num + 1 : 1
+                    }
+                }
+                return {
+                    Log: item['Log'] ? item['Log'] : Log,
+                    Num: item['Num'] ? item['Num'] : 1
+                }
+            })
+            if (!match) {
+                newOb.push({ Log, Num: 1 })
+            }
+            console.log(newOb)
+            const obj = await Object.findOneAndUpdate({ Id: Id }, { $set: { Logs: newOb } }, { new: true })
+            res.json(obj)
+            return
+        }
+
         const object = await Object.findOneAndUpdate(
             { Id: Id },
-            { $push: { Logs: Log } },
+            { $set: { Logs: [...ob['Logs'], { Log, Num: 1 }] } },
             { new: true }
         )
         res.json(object)
